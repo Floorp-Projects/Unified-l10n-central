a823fb3008a1ae3461224774be60018dd8779915
Ryosuke Asano <s1F102400011@iniad.org>
2024-08-15 18:19:29 +0900

    Remove files

--- update-langpack.sh
+++ update-langpack.sh
@@ -7,12 +7,24 @@ echo "Updating language pack for $langId"
 
 cd ./$langId
 
-if [ ! -d .git ]; then
-    git init
-    git remote add upstream hg://hg.mozilla.org/l10n-central/$langId
-    git remote set-url upstream hg://hg.mozilla.org/l10n-central/$langId
-fi
+keep_files=("browser/browser/floorp.ftl")
+find . -type f | while read -r file; do
+    delete=true
+    for keep_file in "${keep_files[@]}"; do
+        if [[ "$file" == "./$keep_file" ]]; then
+            delete=false
+            break
+        fi
+    done
+    if [ "$delete" = true ]; then
+        rm "$file"
+        echo "Deleted: $file"
+    fi
+done
 
+git init
+git remote add upstream hg://hg.mozilla.org/l10n-central/$langId
+git remote set-url upstream hg://hg.mozilla.org/l10n-central/$langId
 git pull upstream branches/default/tip
 
 cd $currentDir
